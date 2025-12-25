1f18e1c92c49b33c18666f20b829ca1c67534fbe
GhostedGaming <epicrobloxgamer41@gmail.com>
2025-12-25 13:07:42 -0500

    Better isr128

--- kernel/applications/sys.h
+++ kernel/applications/sys.h
@@ -1,16 +1,14 @@
 #ifndef SYS_H
 #define SYS_H
-
 #include <stdint.h>
 
 #define SYSCALL(n, a1, a2, a3) ({ \
     int64_t _ret; \
     __asm__ volatile ( \
-        "mov %4, %%r8\n\t" \
         "int $0x80" \
         : "=a"(_ret) \
-        : "a"((uint64_t)(n)), "b"((uint64_t)(a1)), "c"((uint64_t)(a2)), "r"((uint64_t)(a3)) \
-        : "r8", "memory", "cc" \
+        : "a"((uint64_t)(n)), "b"((uint64_t)(a1)), "c"((uint64_t)(a2)), "d"((uint64_t)(a3)) \
+        : "memory", "cc" \
     ); \
     _ret; \
 })
